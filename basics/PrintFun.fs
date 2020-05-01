f710f97874cf03aef698a4910ddc75ad8b513841
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-01 17:10:01 +0200

    Create PrintFun.fs

---
+++ basics/PrintFun.fs
@@ -0,0 +1,8 @@
+printfn "The bool is %b" true
+printfn "The char is %c" 'F'
+printfn "The string is %s" "falcon or \"falcon\" "
+printfn "The int is %i" -3                        // signed
+printfn "The int is %d" 42                        // same with %i
+printfn "The float is %f" 42.0                    // signed
+printfn "The HEX is %X" 42                        // upperccase A-E
+printfn "The float is %e" 0.0000042               // in the form 4.2e-6
