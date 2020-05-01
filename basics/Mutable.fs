11dd33a5be3d788c821948213f545824d6a6c05c
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-01 17:14:19 +0200

    Create Mutable.fs

---
+++ basics/Mutable.fs
@@ -0,0 +1,14 @@
+let mutable x = 10
+let y = 20
+
+printfn "x: %i" x
+printfn "y: %i" y
+
+x <- 15 // <- instead of =
+
+printfn "-----------------------"
+
+printfn "x: %i" x
+printfn "y: %i" y
+
+    
