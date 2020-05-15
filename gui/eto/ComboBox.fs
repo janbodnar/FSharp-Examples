7d77455b89aa5df83a9e3657c4b17d0a4ffc40ca
Jan Bodnar <jan.bodnar@gmail.com>
2020-05-15 17:46:31 +0200

    Create ComboBox.fs

---
+++ gui/eto/ComboBox.fs
@@ -0,0 +1,39 @@
+open System
+open Eto.Forms
+open Eto.Drawing
+
+type MyForm() as this =
+    inherit Form()
+    
+    do
+        this.ClientSize <- Size(600, 400)
+        this.Title <- "Table Layout"
+
+        let dropdown = new DropDown()
+        let label = new Label(Text = "...")
+        
+        ["falcon"; "rock"; "sky"; "cloud"]  |> List.iter dropdown.Items.Add
+
+        dropdown.SelectedValueChanged.Add(fun e -> 
+            label.Text <- dropdown.SelectedValue.ToString()
+            )
+
+        let layout = 
+            new TableLayout(
+                Spacing = Size(15, 5), Padding = Padding(10)
+            )
+        [
+            TableRow(
+                TableCell(dropdown, true), 
+                TableCell(label, false)
+            )
+            TableRow(ScaleHeight = true)
+        ] |> List.iter layout.Rows.Add
+
+        this.Content <- layout
+        
+
+[<EntryPoint;STAThread>]
+let main argv = 
+    (new Application()).Run(new MyForm())
+    0
