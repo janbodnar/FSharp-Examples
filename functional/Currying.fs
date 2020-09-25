9e7378718a9dff3d80ff8812550bc60aa62b1337
Jan Bodnar <jan.bodnar@gmail.com>
2020-09-25 16:07:32 +0200

    Update Currying.fs

--- functional/Currying.fs
+++ functional/Currying.fs
@@ -1,11 +1,14 @@
+//  currying with partial application
+
 let addTwoNumbers x y = x + y
 let add7ToNumber = addTwoNumbers 7
 
 printfn "%d" (add7ToNumber 5)
 
-// Every function in F# has one parameter and returns one value. That value can
-// be of type unit, designated by (), which is similar in concept to void in
-// some other languages.
-// When you have a function that appears to have more than one parameter, F#
-// treats it as several functions, each with one parameter, that are then
-// "curried" to come up with the result we want. 
+(* --------------------------- *)
+
+let add a b =
+    a + b
+
+printfn "%d" (add 2 3)
+printfn "%d" ((add 2) 3)
