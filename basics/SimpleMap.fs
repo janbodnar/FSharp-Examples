46707b76c0b796d90aee1b6d03b58741630cb4c8
Jan Bodnar <jan.bodnar@gmail.com>
2021-11-27 13:18:34 +0100

    Update SimpleMap.fs

--- basics/SimpleMap.fs
+++ basics/SimpleMap.fs
@@ -3,8 +3,5 @@ let vals = [1..5]
 let double input = input |> List.map (fun x -> x * 2)
 vals |> double |> printf "%A\n"
 
-let output = if true then "yes" else "no"
-printf "%A\n" output
-
 let triple = List.map((*) 3)
 vals |> triple |> printf "%A\n"
