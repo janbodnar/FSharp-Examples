034ba2d70678b5b65a9b3eb4b80e6bc1e922da65
Jan Bodnar <jan.bodnar@gmail.com>
2021-11-27 14:54:05 +0100

    Update random_data.fsx

--- basics/random_data.fsx
+++ basics/random_data.fsx
@@ -1,18 +1,15 @@
 open System
 
-[<EntryPoint>]
-let main argv =
+let rand = Random()
 
-    let rand = Random()
+let randomVals = seq { while true do yield rand.Next(100) }
 
-    let randomVals = seq { while true do yield rand.Next(100) }
+let firstTenRandom = 
+    randomVals
+    |> Seq.truncate 10
+    |> Seq.sort
+    |> Seq.toList
 
-    let firstTenRandom = 
-        randomVals
-        |> Seq.truncate 10
-        |> Seq.sort
-        |> Seq.toList
+printfn "%A" firstTenRandom
 
-    printfn "%A" firstTenRandom
-
-    0 
+    
