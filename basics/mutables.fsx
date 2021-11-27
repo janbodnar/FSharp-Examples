f72408b6780f04d3ead2c590b8c227d3fe0f8aed
Jan Bodnar <jan.bodnar@gmail.com>
2021-11-27 14:56:35 +0100

    Update and rename Mutable.fs to mutables.fsx

--- basics/mutables.fsx
+++ basics/mutables.fsx
@@ -11,4 +11,18 @@ printfn "-----------------------"
 printfn "x: %i" x
 printfn "y: %i" y
 
-    
+// arrays are mutable
+
+let vals = [|
+    1
+    2
+    3
+    4
+|]
+
+vals[0] <- 11
+vals[1] <- 12
+vals[2] <- 13
+vals[3] <- 14
+
+printfn "%A" vals
