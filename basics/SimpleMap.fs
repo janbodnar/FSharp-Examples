61fc13be7fcaddc75c581ae0be3e70a45312ce6c
Jan Bodnar <jan.bodnar@gmail.com>
2021-11-27 13:15:50 +0100

    Update SimpleMap.fs

--- basics/SimpleMap.fs
+++ basics/SimpleMap.fs
@@ -3,7 +3,6 @@ let vals = [1..5]
 let double input = input |> List.map (fun x -> x * 2)
 vals |> double |> printf "%A\n"
 
-
 let user = {| FirstName = "John Doe"; Age = 33 |} // Anonymous record
 let updated = {| user with Age = 34 |}
 
@@ -13,8 +12,5 @@ printf "%A\n" updated
 let output = if true then "yes" else "no"
 printf "%A\n" output
 
-// F# works like maths - if something is on both sides 
-// of the = it can often be ommited, giving an even terser syntax
 let triple = List.map((*) 3)
-
 vals |> triple |> printf "%A\n"
