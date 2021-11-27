26f89c85d610c23166b73d0ec768af3e541f87f5
Jan Bodnar <jan.bodnar@gmail.com>
2021-11-27 13:16:40 +0100

    Update SimpleMap.fs

--- basics/SimpleMap.fs
+++ basics/SimpleMap.fs
@@ -3,12 +3,6 @@ let vals = [1..5]
 let double input = input |> List.map (fun x -> x * 2)
 vals |> double |> printf "%A\n"
 
-let user = {| FirstName = "John Doe"; Age = 33 |} // Anonymous record
-let updated = {| user with Age = 34 |}
-
-printf "%A\n" user
-printf "%A\n" updated
-
 let output = if true then "yes" else "no"
 printf "%A\n" output
 
